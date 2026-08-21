21a79a63a7b27af0f7a3c9783e49f27429388741
Dipu Kumar <dipukumarpojhi@gmail.com>
2026-08-21 11:14:19 +0530

    Connected mongodb into this project

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import contactRoutes from "./routes/contactRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
+import connectDB from "./config/dbConnection.js";
 
 const app = express();
 
 dotenv.config();
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
